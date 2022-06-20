385e02a72fe7c93d1eefbc55130a2b483e9636df
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-06-21 00:29:14 +0900

    미사용 import 제거

--- src/main/java/com/hyeok/back/Member/repository/MemberRepository.java
+++ src/main/java/com/hyeok/back/Member/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.hyeok.back.Member.repository;
 
 import com.hyeok.back.Member.Entity.MemberEntity;
-import com.hyeok.back.Member.dto.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
