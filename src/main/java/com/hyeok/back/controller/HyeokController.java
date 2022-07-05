8de6ba26c781a8acc441fc0ab2599cdaa3e16652
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-07-05 23:46:24 +0900

    임시커밋 - 로그 추가

--- src/main/java/com/hyeok/back/controller/HyeokController.java
+++ src/main/java/com/hyeok/back/controller/HyeokController.java
@@ -10,17 +10,14 @@ import com.hyeok.back.hyeokException.HyeokException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @Tag(name = "로그인 / 회원가입")
 @Slf4j
 @RestController
-@RequestMapping(path = "/member")
+@RequestMapping(path = "/member", method = RequestMethod.POST)
 public class HyeokController {
 
     @Resource
@@ -29,6 +26,7 @@ public class HyeokController {
     @Operation(summary = "로그인", description = "아이디, 비밀번호 입력")
     @PostMapping(path = "/login")
     public HyeokApi memberLogin(@RequestBody LoginReq loginReq) {
+        log.info("요청값 : {}", loginReq);
         if(memberService.selectJoin(loginReq.getUserId(), loginReq.getPassword())) {
             log.info(ApiCode.API_0000.getMsg());
             return new HyeokApi(new CommonResult(ApiCode.API_0000.getCode(), ApiCode.API_0000.getMsg()));
@@ -43,6 +41,7 @@ public class HyeokController {
     @Operation(summary = "회원가입", description = "아이디, 비밀번호, 이름 입력")
     @PostMapping(path = "/signup")
     public HyeokApi memberJoin(@RequestBody SignUpReq signUpReq) {
+        log.info("요청값 : {}", signUpReq);
         if(memberService.saveJoin(signUpReq)) {
             log.info(ApiCode.API_0000.getMsg());
             return new HyeokApi(new CommonResult(ApiCode.API_0000.getCode(), ApiCode.API_0000.getMsg()));
